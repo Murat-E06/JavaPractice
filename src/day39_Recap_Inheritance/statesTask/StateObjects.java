80e0c5ac0c90b22171babcd11a431983ab5a1691
Murat <laserdancer06@gmail.com>
2022-04-18 23:12:59 +0300

    State Task

--- src/day39_Recap_Inheritance/statesTask/StateObjects.java
+++ src/day39_Recap_Inheritance/statesTask/StateObjects.java
@@ -6,5 +6,9 @@ public class StateObjects {
         Virginia virginia = new Virginia("Alcohol", "DP", "AP", "Adam McCarty", 2300000, 0.25, 17, true);
         System.out.println(virginia);
         virginia.drive();
+        System.out.println(virginia.getPopulation());
+        System.out.println("----------------------------------------");
+        virginia.setPopulation(11000000);
+        System.out.println(virginia);
     }
 }
